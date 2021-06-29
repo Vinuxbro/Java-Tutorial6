8127856598c162a28fd2142e1667f98f5386a5c9
Vinuka Aksika <vinukawi10@gmail.com>
2021-06-29 21:00:05 +0530

    Initial commit

--- src/Account.java
+++ src/Account.java
@@ -4,35 +4,43 @@ public class Account {
     private String accountType;
     private double currentBalence;
 
-    public Account(String holderName, int accountNumber, String accountType, double currentBalence) {
+    public Account(String holderName, int accountNumber, String accountType, double currentBalence) { //q1,q2
         this.holderName = holderName;
         this.accountNumber = accountNumber;
         this.accountType = accountType;
         this.currentBalence = currentBalence;
     }
 
-    void depositMoney(double deposit) {
+    void depositMoney(double deposit) {         //q1,q2
         this.currentBalence = currentBalence + deposit;
     }
 
-    void withdrawMoney(double withdraw) {
+    void withdrawMoney(double withdraw) {      //q1,q2
         this.currentBalence = currentBalence - withdraw;
     }
 
-    void displayAccountDetails() {
+    void displayAccountDetails() {            //q1,q2
         System.out.println("Name : " + holderName);
         System.out.println("Account Type : " + accountType);
         System.out.println("Account Number : " + accountNumber);
 
     }
-    void setAccountType(String accountType){
+    void setAccountType(String accountType){    //q2
         this.accountType=accountType;
     }
-    void setHolderName(String holderName){
+    void setHolderName(String holderName){    //q2
         this.holderName=holderName;
     }
 
-    public double checkBalence() {
+    void setAccountNumber(int accountNumber){
+        this.accountNumber=accountNumber;
+    }
+
+    void setCurrentBalence(double currentBalence){
+        this.currentBalence=currentBalence;
+    }
+
+    public double checkBalence() {   //q1,q2
         return currentBalence;
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Account acc1= new Account("John",23654,"Current",600);
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter Account Holder's Name : ");
+        String AccName = input1.nextLine();
+        acc1.setHolderName(AccName);
+
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter Account Number : ");
+        int AccNumber = input2.nextInt();
+        acc1.setAccountNumber(AccNumber);
+
+
+
 
     }
 }
