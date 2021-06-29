24e06a30408404f2c0b74f28b8946ce532078b24
Vinuka Aksika <vinukawi10@gmail.com>
2021-06-29 21:28:03 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,20 @@ public class Main {
         int AccNumber = input2.nextInt();
         acc1.setAccountNumber(AccNumber);
 
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("Enter account initial Balance : ");
+        double Accbalance = input3.nextDouble();
+        acc1.setCurrentBalence(Accbalance);
+
+        Scanner input4 = new Scanner(System.in);
+        System.out.println("Enter Account type : ");
+        String AccType= input4.nextLine();
+        acc1.setAccountType(AccType);
+
+        System.out.println("Creating "+AccName+"'s Account....." );
+
+
+
 
 
 
