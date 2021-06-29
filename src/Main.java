635d21ee4289439fc24b7942531398a178a0e148
Vinuka Aksika <vinukawi10@gmail.com>
2021-06-29 21:28:03 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int option=0;
+
+
         Account acc1= new Account("John",23654,"Current",600);
         Scanner input1 = new Scanner(System.in);
         System.out.println("Enter Account Holder's Name : ");
@@ -24,7 +27,93 @@ public class Main {
         String AccType= input4.nextLine();
         acc1.setAccountType(AccType);
 
-        System.out.println("Creating "+AccName+"'s Account....." );
+        System.out.println("\n\nCreating "+AccName+"'s Account....." );
+
+        System.out.println("To Withdraw Money        :Press 1");
+        System.out.println("To Deposit Money         :Press 2");
+        System.out.println("To Check Balance         :Press 3");
+        System.out.println("To View Account Details  :Press 4");
+        System.out.println("To Exit                  :Press 5");
+
+
+        while (option != 5) {
+
+            Scanner input5 = new Scanner(System.in);
+            option = input5.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.println("Enter your withdraw : ");
+                    Scanner input6 = new Scanner(System.in);
+                    double AccWithdraw = input6.nextDouble();
+                    acc1.withdrawMoney(AccWithdraw);
+                    System.out.println("To Withdraw Money        :Press 1");
+                    System.out.println("To Deposit Money         :Press 2");
+                    System.out.println("To Check Balance         :Press 3");
+                    System.out.println("To View Account Details  :Press 4");
+                    System.out.println("To Exit                  :Press 5");
+                    break;
+
+
+
+                case 2:
+                    System.out.println("Enter your deposit : ");
+                    Scanner input7 = new Scanner(System.in);
+                    double AccDeposit = input7.nextDouble();
+                    acc1.depositMoney(AccDeposit);
+                    System.out.println("To Withdraw Money        :Press 1");
+                    System.out.println("To Deposit Money         :Press 2");
+                    System.out.println("To Check Balance         :Press 3");
+                    System.out.println("To View Account Details  :Press 4");
+                    System.out.println("To Exit                  :Press 5");
+                    break;
+
+
+                case 3:
+                    System.out.println("Your current balance is : "+acc1.checkBalence());
+                    System.out.println("To Withdraw Money        :Press 1");
+                    System.out.println("To Deposit Money         :Press 2");
+                    System.out.println("To Check Balance         :Press 3");
+                    System.out.println("To View Account Details  :Press 4");
+                    System.out.println("To Exit                  :Press 5");
+                    break;
+
+
+                case 4:
+                    acc1.displayAccountDetails();
+                    System.out.println("To Withdraw Money        :Press 1");
+                    System.out.println("To Deposit Money         :Press 2");
+                    System.out.println("To Check Balance         :Press 3");
+                    System.out.println("To View Account Details  :Press 4");
+                    System.out.println("To Exit                  :Press 5");
+                    break;
+
+                case 5:
+                    System.out.println("........THANK YOU...........");
+                    break;
+                default:
+                    System.out.println("Invalid Option!!!!!!! Please enter valid option.....");
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
 
 
 
